8bed2410ef8a5ac18c33da57973a2fbdd91b1738
ihorhladkov <ihor.hladkov@gmail.com>
2023-11-24 13:52:59 +0200

    added skeleton

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,29 +1,22 @@
+"use client";
+
 import { useQuery } from "@tanstack/react-query";
 import { Inter } from "next/font/google";
 import { trpc } from "../utils/trpc";
 import { getOptionForVote, getRandomPokemon } from "../utils/getRandomPokemon";
-import { useState } from "react";
+import { Suspense, useState } from "react";
 import Image from "next/image";
 
 const inter = Inter({ subsets: ["latin"] });
 
-interface Props {
-  firstPokemonId: number;
-  secondPokemonId: number;
-}
+// interface Props {
+//   firstPokemonId: number;
+//   secondPokemonId: number;
+// }
 
-export default function Home(props: Props) {
+export default function Home() {
   const [ids, updateIds] = useState(() => getOptionForVote());
   const [firstPokemonId, secondPokemonId] = ids;
-
-  const { data: firstPokemon, isLoading: firstPokemonIsLoading } =
-    trpc.getPokemonById.useQuery({
-      id: +firstPokemonId,
-    });
-  const { data: secondPokemon, isLoading: secondPokemonIsLoading } =
-    trpc.getPokemonById.useQuery({
-      id: +secondPokemonId,
-    });
 
   const { mutate: voteMutate } = trpc.voteCast.useMutation();
 
@@ -36,56 +29,61 @@ export default function Home(props: Props) {
     updateIds(getOptionForVote());
   };
 
-  if (firstPokemonIsLoading || secondPokemonIsLoading) {
-
-    return <div>Loading...</div>;
-  }
-
   return (
     <div className="h-screen w-screen flex flex-col justify-center items-center">
       <div className="text-2xl text-center">Which Pokemon is Rounder?</div>
       <div className="p-2"></div>
-      <div className="border rounded p-8 flex justify-between items-center max-w-2xl">
-        {!firstPokemonIsLoading &&
-          firstPokemon &&
-          !secondPokemonIsLoading &&
-          secondPokemon && (
-            <>
-              <PokemonListing
-                nickname={firstPokemon.nickname}
-                sprites={firstPokemon.spritesUrl}
-                vote={() => onVoteRoundest(firstPokemonId)}
-              />
-              <div className="p-8">VS</div>
-              <PokemonListing
-                nickname={secondPokemon.nickname}
-                sprites={secondPokemon.spritesUrl}
-                vote={() => onVoteRoundest(secondPokemonId)}
-              />
-            </>
-          )}
+      <div className="border rounded p-8 grid grid-cols-3 items-center justify-center w-[400px] h-[300px]  max-w-2xl">
+        <Suspense fallback={<p>Loading...</p>}>
+          <PokemonListing
+            pokemonId={firstPokemonId}
+            vote={() => onVoteRoundest(firstPokemonId)}
+          />
+        </Suspense>
+        <div className="mx-auto p-8">VS</div>
+        <Suspense fallback={<p>Loading...</p>}>
+          <PokemonListing
+            pokemonId={secondPokemonId}
+            vote={() => onVoteRoundest(secondPokemonId)}
+          />
+        </Suspense>
       </div>
     </div>
   );
 }
 
 interface PropsPokemon {
-  nickname: string;
-  sprites: string | null;
+  pokemonId: number;
   vote: () => void;
 }
 
-export const PokemonListing: React.FC<PropsPokemon> = ({
-  nickname,
-  sprites,
-  vote,
-}) => {
+export const PokemonListing: React.FC<PropsPokemon> = ({ pokemonId, vote }) => {
+  const { data: pokemon, isLoading } = trpc.getPokemonById.useQuery({
+    id: +pokemonId,
+  });
+
   return (
     <div>
-      <Image width={100} height={100} alt="Pokemon Ebalo" src={`${sprites}`} />
-      <div className="text-xl text-center capitalize">{nickname}</div>
+      {pokemon ? (
+        <>
+          <Image
+            width={100}
+            height={100}
+            alt="Pokemon Ebalo"
+            src={`${pokemon?.spritesUrl}`}
+          />
+          <div className="text-xl text-center capitalize">
+            {pokemon?.nickname}
+          </div>{" "}
+        </>
+      ) : (
+        <>
+          <div className="animate-pulse rounded-full bg-orange-900 h-[96px] w-full mb-1" />
+          <div className="animate-pulse rounded-md bg-orange-900 h-[24px] w-full" />
+        </>
+      )}
       <button
-        className="bg-blue-500 w-full mx-auto hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        className="bg-blue-500 w-full mx-auto hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-1"
         onClick={vote}
       >
         Vote
